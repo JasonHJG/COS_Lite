6855ceafa21f4f887f528c0c32be27897a3283d3
kingther94c <kingther94c@gmail.com>
2018-11-28 13:40:08 -0500

    backtest&test

--- BackTester/backtest.py
+++ BackTester/backtest.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 from BackTester import utils
 from IPython.display import display, HTML
-
+from Data import Trade_book
 
 class BackTester():
     """
@@ -19,13 +19,50 @@ class BackTester():
         self.price_process = price_process
         self.player = player
         self.df_trade = None
+        self.trade_book = None
 
-    def backtest(self, trading_cost, total_time=None, initial_value=1e5, ):
+    def trade_one_step(self, trading_cost, utility_function, strategy, trade_book, threshold, action, epsilon=.001):
+        """
+        trade one step forward
+        :param trading_cost:
+        :param utility_function:
+        :param strategy:
+        :param trade_book:
+        :param threshold:
+        :param action:
+        :param epsilon:
+        :return:
+        """
+        self.trade_book = trade_book
+        # get the player's state at t
+        time_step, price, position = trade_book.get_recent_state()
+        state = [price, position]
+        # need to find the possible actions first
+        possible_actions = []
+        for action in action:
+            if threshold[0] <= action + position <= threshold[1]:
+                possible_actions.append(action)
+        action = strategy.epsilon_greedy(state, possible_actions, epsilon)
+        trade_book.add_action(time_step, action)
+        self.price_process.move_forward()
+        # update price and position at t+1
+        next_time_step, next_price = self.price_process.get_current_price()
+        next_position = position + action
+        trade_book.add_state(next_time_step, next_price, next_position)
+        # add player's value at t
+        #dv = next_position * (next_price - price) - trading_cost(action)
+        #utility = utility_function(dv)
+        #trade_book.add_value(time_step, dv)
+        #trade_book.add_utility(time_step, utility)
+
+
+    def backtest(self, total_time=None, initial_value=1e5, threshold=None, action=None, ):
         """
         backtest for a certain time steps
-        :param trading_cost: function
         :param total_time: int, time steps
         :param initial_value: float, initial total cash for trading
+        :param threshold: position threshold, if None get player.threshold
+        :param action: action space, if None get player.action
         :return:
         """
         # if time == None, get price_process's total_time
@@ -36,20 +73,27 @@ class BackTester():
             except AttributeError:
                 pass
 
-        # replace the player's observable process with a new price_process
-        self.player.price_process = self.price_process
+        if not threshold:
+            threshold = self.player.threshold
 
-        # trade with epsilon .01
+        if not action:
+            action = self.player.action
+
+        # get the last state from training trade_book
+        trade_book = Trade_book()
+        trade_book.add_state(0, self.price_process.current_price, 0)
+        # trade with epsilon 0
         for i in range(total_time):
-            self.player.trade_greedy_one_step(.01)
+            self.trade_one_step(self.player.trading_cost, self.player.utility_function, self.player.strategy,
+                                trade_book, threshold, action, epsilon=.001)
 
         # convert the trade book to a dataframe
-        trade_book = self.player.trade_book.book
-        df_trade = pd.DataFrame([trade_book[i]['state'] for i in sorted(list(trade_book))])
+        book = trade_book.book
+        df_trade = pd.DataFrame([book[i]['state'] for i in sorted(list(book))])
 
         # compute stock, cash and total value
         df_trade.loc[:, "stock"] = df_trade.loc[:, "price"] * df_trade.loc[:, "position"]
-        df_trade.loc[:, "trading_cost"] = trading_cost(df_trade.position.diff())
+        df_trade.loc[:, "trading_cost"] = self.player.trading_cost(df_trade.position.diff())
         # note that action is made based on current price
         d_cash = (df_trade.price.shift() * df_trade.position.diff()).fillna(0) + df_trade.trading_cost
         df_trade.loc[:, "cash"] = initial_value - d_cash.cumsum()
